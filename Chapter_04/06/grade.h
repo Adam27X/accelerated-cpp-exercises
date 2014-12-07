70324ab9425c0c252edfb74d87cd21d5c34913cc
Adam McLaughlin <adam27x@gatech.edu>
2014-12-07 14:20:50 -0500

    Remaining exercises for Chapter 4.

---
+++ Chapter_04/06/grade.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <vector>
+#include "student_info.h"
+
+double grade(double midterm_exam, double final_exam, double homework);
+double grade(double midterm_exam, double final_exam, const std::vector<double> &hw);
