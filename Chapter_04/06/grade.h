40038a7f278eea6a00238215192d8b6d05d10e8d
Adam McLaughlin <Adam27X@gatech.edu>
2015-02-23 18:30:03 -0500

    More refactoring.

--- Chapter_04/06/grade.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <vector>
-#include "student_info.h"
-
-double grade(double midterm_exam, double final_exam, double homework);
-double grade(double midterm_exam, double final_exam, const std::vector<double> &hw);
--- Chapter_07/02/grade.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include "student_info.h"
-
-double grade(double midterm_exam, double final_exam, double homework);
-double grade(double midterm_exam, double final_exam, const std::vector<double> &hw);
-double grade(const student_info &s);
-char grade_letter(const student_info &s);
